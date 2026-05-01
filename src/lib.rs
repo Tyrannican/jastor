cce6b61968df9b150e0197577997ffc790ee51f4
Graham Keenan <graham.keenan@outlook.com>
2026-05-01 17:50:15 +0100

    cleanup of file parsing

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,54 @@ pub mod event;
 pub mod parser;
 pub mod player;
 pub mod types;
+
+use memmap::MmapOptions;
+use parser::{EventLogParser, ParsedEvent};
+use std::{io::BufReader, path::Path};
+pub struct LogFile;
+
+impl LogFile {
+    pub fn parse(path: impl AsRef<Path>) -> eyre::Result<Vec<ParsedEvent>> {
+        let file = std::fs::File::open(path)?;
+
+        // Safety: We can guarantee that the file is not modified underneath
+        // as these are static logs that don't change
+        let map = unsafe { MmapOptions::new().map(&file)? };
+        std::thread::scope(|scope| {
+            let n = std::thread::available_parallelism().expect("a valid value for parallelism");
+            let chunk = map.len() / n.get();
+            let mut at = 0;
+            let (tx, rx) = std::sync::mpsc::sync_channel::<Vec<ParsedEvent>>(n.get());
+
+            for _ in 0..n.get() {
+                let start = at;
+                let end = (at + chunk).min(map.len());
+                let end = if end == map.len() {
+                    map.len()
+                } else {
+                    let buf = &map[end..];
+                    let newline = buf.iter().position(|b| *b == b'\n').unwrap_or(buf.len());
+                    end + newline + 1
+                };
+                let reader = BufReader::new(&map[start..end]);
+                let parser = EventLogParser::new(reader);
+                at = end;
+
+                let tx = tx.clone();
+                scope.spawn(move || {
+                    tx.send(
+                        parser
+                            .into_iter()
+                            .map(|event| event.expect("a valid event"))
+                            .collect::<Vec<ParsedEvent>>(),
+                    )
+                });
+            }
+
+            drop(tx);
+            let mut events = rx.into_iter().collect::<Vec<Vec<ParsedEvent>>>();
+            events.sort_by(|a, b| a[0].timestamp.cmp(&b[0].timestamp));
+            Ok(events.into_iter().flatten().collect::<Vec<ParsedEvent>>())
+        })
+    }
+}
